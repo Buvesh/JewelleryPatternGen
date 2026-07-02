76489b4902a4bd9732e2e8654d2007321ef758a1
Buvesh <buveshb598@gmail.com>
2026-07-02 14:38:57 +0530

    fix: import Navigate from react-router-dom

--- WEBSITE/react/src/App.jsx
+++ WEBSITE/react/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Modern from "./pages/Modern/modern";
 import Traditional from "./pages/Traditional/traditional";
 import ContactUs from "./pages/ContactUs/contactus";
